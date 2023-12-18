5e7108011cee82395c4d8fb187273d4af6bad167
andrey <naumov.a.n@icloud.com>
2023-12-18 23:38:58 +0700

    изменён вывод нашёл как задать имя потоку

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -2,11 +2,11 @@ package ru.gb;
 
 public class Main {
     public static void main(String[] args) {
-        Thread philos1 = new Thread(new Philosophers());
-        Thread philos2 = new Thread(new Philosophers());
-        Thread philos3 = new Thread(new Philosophers());
-        Thread philos4 = new Thread(new Philosophers());
-        Thread philos5 = new Thread(new Philosophers());
+        Thread philos1 = new Thread(new Philosophers(), "Философ 1");
+        Thread philos2 = new Thread(new Philosophers(), "Философ 2");
+        Thread philos3 = new Thread(new Philosophers(), "Философ 3");
+        Thread philos4 = new Thread(new Philosophers(), "Философ 4");
+        Thread philos5 = new Thread(new Philosophers(), "Философ 5");
 
         philos1.start();
         philos2.start();
